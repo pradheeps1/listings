2c26f6d63e308e27dfa5dfe9cd23a4dee0e1c03c
pradheep <pradheep@motorola.com>
2018-01-06 12:34:18 +0530

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import { HeaderComponent } from './listings/header/header.component';
+import { ContentComponent } from './listings/content/content.component';
+import { SearchComponent } from './search/search.component';
+import { ListingsComponent } from './listings/listings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    SearchComponent,
+    ListingsComponent
   ],
   imports: [
     BrowserModule,
