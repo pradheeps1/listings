848c05263c9f5e6be4de60b426c6469c03696ff0
pradheep <pradheep@motorola.com>
2018-01-06 19:17:16 +0530

    optimize

--- src/app/listings/content/content.component.ts
+++ src/app/listings/content/content.component.ts
@@ -20,15 +20,16 @@ export class ContentComponent implements OnInit {
   }
 
   getListingCards() {
-    this.listingsService.getContentListingPage(this.currentPage)
+    this.listingsService.getContentListingPage(this.currentPage++)
       .subscribe((response) => this.processResponseCards(response));
   }
 
   private processResponseCards = (cardsRes) => {
-    let cardsData = cardsRes.json().page['content-items'].content;
-    this.currentPage++;
-    if (this.currentPage === 4)
+    let response = cardsRes.json();
+    let cardsData = response.page['content-items'].content;
+    if (response.page['page-size-requested'] !== response.page['page-size-returned'])
       this.finished = true;
+
     for (let i = 0; i < cardsData.length; i++) {
       this.cards.push(new CardModel(cardsData[i].name, cardsData[i]['poster-image']));
     }
